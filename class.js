9a8dddbbcebd6506ebc8e3e1be1c90d7cc43177e
Bhargav Vanra <88825575+Bhargav007SGL@users.noreply.github.com>
2022-07-29 18:54:01 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,66 @@
+// 1)
+// class User {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     sayHi() {
+//         console.log(this.name);
+//     }
+// }
+
+// class user2 extends User
+// {
+
+// }
+
+// let user = new user2("Bhargav");
+// user.sayHi();
+
+// 2)
+// class Car {
+//     constructor(brand) {
+//         this.carname = brand;
+//     }
+//     present() {
+//         return 'I have a ' + this.carname;
+//     }
+// }
+
+// class Model extends Car {
+//     constructor(brand, mod) {
+//         super(brand);
+//         this.model = mod;
+//     }
+//     show() {
+//         return this.present() + ', it is a ' + this.model;
+//     }
+// }
+
+// mycar = new Model("Ford", "Mustang");
+// console.log(mycar.show());
+
+// 3
+// class Car {
+//     constructor()
+//     {
+        
+//     }
+//     present() {
+//         return 'I have a ' + `Op`;
+//     }
+// }
+
+// class Model extends Car {
+//     constructor(brand, mod) {
+//         super();
+//         this.model = mod;
+//         this.brand = brand;
+//     }
+//     show() {
+//         return this.present() + ', it is a ' + this.brand;
+//     }
+// }
+
+// mycar = new Model("Ford", "Mustang");
+// console.log(mycar.show());
+
